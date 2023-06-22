0eca41345dae3e1e1dd0687a170ab5cc34744561
gamerpro861 <137376327+gamerpro861@users.noreply.github.com>
2023-06-22 16:28:23 +0300

    Update and rename assets/js/beko_admin.js to skibidi toilet hunt.js
    benim oyunumun adı Skibidi Toilet Hunt

---
+++ skibidi toilet hunt.js
@@ -0,0 +1,13 @@
+echo off
+ this game name is skibidi toilet hunt this games in the www.google.com
+     this games match games
+    this around the turkish games
+this games my roblox caracthers says turn around
+and characters players go to dhe gamer  behind 
+ and gamers turn around
+   and players died and go to the lobys
+this game a match points what time not to died and games finished gamers 1000000000000 points
+ this games elemeneted to go home screen
+and this game not the screenshot
+this gamers not the hack and maryys
+(program finished)
